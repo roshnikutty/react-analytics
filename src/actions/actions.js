2ef1f5cb613fd287ddcdc4628be769d965f604bf
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-11-18 14:03:12 -0500

    requests made to api url

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,16 @@
 // import curl from 'curl';
-// import $ from 'jquery';
+import $ from 'jquery';
+import fetchJsonp from 'fetch-jsonp';
 import { fromByteArray } from 'base64-js';
 import { TextEncoder } from 'text-encoding';
 
+global.fetchJsonp = fetchJsonp;
 const api_key = `API_KEY`;
 const api_secret = `API_SECRET`;
 const API_URL = `https://api.localytics.com/v1`;
+
+//get current date;
+let currentDate = new Date().toISOString().substring(0, 10);
 
 const base64EncodingUTF8 = str => {
     const encoded = new TextEncoder('utf-8').encode(str);
@@ -25,22 +30,98 @@ export const getAnalytics = () => {
     //Thunk function
     console.log(token)
     return function (dispatch) {
-        return fetch("https://api.localytics.com/v1/", {
+        return fetch("https://api.localytics.com/v1/apps?api_key=API_KEY&api_secret=API_SECRET", {
             method: 'GET',
             headers: {
                 'Authorization': `Basic ${token}`,
                 'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
             },
-            // mode: 'cors'
+            mode: 'no-cors'
         }).then(res => {
-            console.log(res)
             return res.json()
-        }).then((analyticsJson) => {
-            console.log("HELLO")
+        })
+        .then((analyticsJson) => {
+            console.log(analyticsJson)
             dispatch(getAnalyticsSuccess(analyticsJson))
-        }).catch(err =>
+        })
+        .catch(err =>
             console.log(`Error while running getAnalytics: ${err}`)
             )
+        // .catch(err =>
+        //     console.log(`Error while running getAnalytics: ${err}`)
+        //     )
+        //---------------
+        // return global.fetchJsonp(`${API_URL}`, {
+        //         method: 'GET',
+        //         headers: {
+        //             'Authorization': `Basic ${token}`,
+        //             'Access-Control-Allow-Origin': '*',
+        //             'Content-Type': 'application/json',
+        //             'Accept':'application/vnd.localytics.v1+hal+json'
+        //         },
+        //         mode: 'no-cors'
+        // })
+        // .then(data => {
+        //     if (!data.ok) {
+        //         return Promise.reject(data.statusText);
+        //     }
+        //     return data.json()
+        // }).then((analyticsJson) => {
+        //     console.log(analyticsJson)
+        //     dispatch(getAnalyticsSuccess(analyticsJson))
+        // }).catch(err =>
+        //     console.log(`Error while running getLandingPageJobs(): ${err}`)
+        // )
+        //--------
+        // return $.ajax({
+        //     url: API_URL,
+        //     method: 'GET',
+        //     headers: {
+        //         'Authorization': `Basic ${token}`,
+        //         'Access-Control-Allow-Origin': '*',
+        //         'Content-Type': 'application/json',
+        //         'Accept': 'application/vnd.localytics.v1+hal+json'
+        //     },
+        //     mode: 'no-cors'
+        // })
+        // return $.ajax({  
+        //     url: API_URL,
+        //     username : api_key,
+        //     password :api_secret,
+        //     type: 'GET',
+        //     contentType: 'application/x-www-form-urlencoded',
+        //     // dataType: "text",
+        //     xhrFields: 
+        //     {
+        //         withCredentials: true
+        //     },
+        //     beforeSend: function (xhr) { 
+        //         xhr.setRequestHeader('Authorization', 'Basic ' + btoa(api_key + ":" + api_secret));             
+        //     }
+        // })
+        // .done((analyticsJson) => {
+        //     console.log(analyticsJson)
+        //     dispatch(getAnalyticsSuccess(analyticsJson))
+        // }).fail(err =>
+        //     console.log(`Error while running getLandingArcles: ${err}`)
+        //     )
+        //=========
+        // var settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://api.localytics.com/v1/apps?api_key=API_KEY&api_secret=API_SECRET",
+        //     "method": "GET",
+        //     "headers": {
+        //       "authorization": "Basic QVBJX0tFWTpBUElfU0VDUkVU",
+        //       "cache-control": "no-cache"
+        //     //   "postman-token": "ddf094dd-1ccc-ad64-a857-451ec187d0fa"
+        //     }
+        //   }
+          
+        //   $.ajax(settings).done(function (response) {
+        //     console.log(response);
+        //   });
     }
 }
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,9 +1,20 @@
 import { GET_ANALYTICS_SUCCESS } from '../actions/actions'
 
 let initialState = {
+    app_id: ""
 };
 
 export const analyticsReducer = (state = initialState, action) => {
-    
+    // let app_id;
+    // if (action.type === GET_ANALYTICS_SUCCESS) {
+    //     let appsResponse = action.payload;
+    //     if (appsResponse._embedded.apps.length) {
+    //         let appStopShop = appsResponse._embedded.apps.filter((app) => {
+    //             return app.name === `1 Stop Shop`;
+    //         })
+    //         app_id = appStopShop.app_id;
+    //     }
+    //     return Object.assign({}, state, { app_id });
+    // }
     return state;
 }
